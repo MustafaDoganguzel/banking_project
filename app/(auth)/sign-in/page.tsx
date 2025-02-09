9f7643fdbae052a32f2945022938c103a6220074
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-10 01:59:10 +0300

    Implement the Auth Layout
    Sign-In
    Sign-Up completed

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignIn = () => {
     return (
-        <div>SignIn</div>
+        <section className='flex-center size-full max-sm:px-6'>
+            <form action=""></form>
+            <AuthForm type='sign-in' />
+        </section>
     )
 }
 
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,16 @@
+'use server';
+
+export const singIn = async () => {
+    try {
+
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // create a user acc
+    } catch (error) {
+        console.log('Error', error)
+    }
+}
